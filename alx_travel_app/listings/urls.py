1ed9b93ae26e769c6d2f17f514777fb642ba24dd
dagem12 <0zerocool99@gmail.com>
2025-05-18 20:23:34 +0300

    travel app

--- alx_travel_app/listings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views  # or from listings import views if absolute
-
-urlpatterns = [
-    # example path
-    path('', views.index, name='listings-index'),
-]
